5363ce2b6cb25d4d47a286ba9fb817121ec4eb78
amsem <saidamine.selmani@fgei.ummto.dz>
2023-10-14 08:23:40 +0100

    fix: DB connection with env variable

--- helper/models.go
+++ helper/models.go
@@ -2,24 +2,16 @@ package helper
 
 import (
 	"database/sql"
-	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
-)
-// "am53m*********"
 
-const (
-    host = "127.0.0.1"
-    port = 5432
-    user = "amsem"
-    password = "am53m*********" 
-    dbname = "mydb"
 )
+
+var connectString = os.Getenv("CONNSTR")
 
 func InitDB() (*sql.DB, error) {
-    var connectString = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-    
     db, err := sql.Open("postgres", connectString)
     if err != nil {
         return nil, err
